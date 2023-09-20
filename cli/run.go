5a4b661feb77fe75a3a633afce967ba4cd1f2d7c
Abhi Chowdary <abhilashnunna584@gmail.com>
2023-09-20 18:43:00 -0400

    Added comments to the Run function

--- cli/run.go
+++ cli/run.go
@@ -1,21 +1,24 @@
 package cli
 
 import (
-	argsLib "github.com/taubyte/tau-cli/cli/args"
-	"github.com/taubyte/tau-cli/i18n"
+        argsLib "github.com/taubyte/tau-cli/cli/args"
+        "github.com/taubyte/tau-cli/i18n"
 )
-
+// Run is the entry point for executing CLI commands.
+// It accepts a variadic argument list of strings representing the command-line arguments.
+// It creates a new CLI application, parses the command-line arguments, and runs the specified command.
 func Run(args ...string) error {
-	app, err := New()
-	if err != nil {
-		return i18n.AppCreateFailed(err)
-	}
+        app, err := New()
+        if err != nil {
+                return i18n.AppCreateFailed(err)
+        }
 
-	if len(args) == 1 {
-		return app.Run(args)
-	}
+        if len(args) == 1 {
+                return app.Run(args)
+        }
+// ParseArguments function is used to parse and prepare the command-line arguments.
+        args = argsLib.ParseArguments(app.Flags, app.Commands, args...)
 
-	args = argsLib.ParseArguments(app.Flags, app.Commands, args...)
-
-	return app.Run(args)
+        return app.Run(args)
 }
+
